4918e9433ee06f28752c5a369bba9f2638425920
blackanthrax <blackanthrax666@gmail.com>
2016-10-07 16:26:32 +0200

    initial commit

---
+++ src/main/java/com/github/blackanthrax/twitchbot/plugin/TwitchbotPlugin.java
@@ -0,0 +1,8 @@
+package com.github.blackanthrax.twitchbot.plugin;
+
+import org.springframework.plugin.core.Plugin;
+import org.springframework.plugin.metadata.PluginMetadata;
+
+public interface TwitchbotPlugin extends Plugin<PluginMetadata>{
+
+}
